d56da28d4c3c732fa1e8e69bb4734ac6eb8dc7d2
utkarsh-kharche-dax <utkarsh.kharche@data-axle.com>
2022-07-31 10:03:40 +0530

    .net Assignment Pushed

---
+++ NET/test/test/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using ASPNetCoreIdentityCustomFields.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace test.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.firstName)
+                .HasMaxLength(250);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.lastName)
+                .HasMaxLength(250);
+
+        }
+    }
+}
---
+++ NET/test/test/Data/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace ASPNetCoreIdentityCustomFields.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
